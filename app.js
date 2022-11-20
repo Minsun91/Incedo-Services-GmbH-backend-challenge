935197bae99869a68091a87efff6ec3c06c4ebf6
Minsun91 <minsunkim167@gmail.com>
2022-11-20 20:12:36 +0100

    CREATE: csv file

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = require("./router");
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+app.use("/find", router);
+app.listen(port, () => {
+    console.log(`
+    ################################################
+    🛡️  Server listening on port: ${port} 🛡️
+    ################################################`);
+});
--- artist.js
+++ artist.js
@@ -1,27 +1,73 @@
 require("dotenv").config();
+const exportToCsv = require("export-to-csv");
 const fetch = require("node-fetch");
-const axios = require("axios");
+const express = require("express");
+const fs = require("fs");
+const json2csv = require("json2csv");
+const stringify = require("csv-stringify");
 
-const url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=${process.env.mykey}&format=json`;
+module.exports = new (class findInfo {
+    artistInfo = async (req, res) => {
+        // const artistName = document.getElementById("input");
+        const { artistName } = req.params;
+        const url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artistName}&api_key=${process.env.mykey}&format=json`;
+        let response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let artists = await response.json();
+        let artistInfos = [];
 
-const artistInfo = fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        // console.log(
-        //     data.results.artistmatches.artist[0].mbid,
-        //     data.results.artistmatches.artist.length
-        // );
-
-        for (let i = 0; i < data.results.artistmatches.artist.length; i++) {
-            const artistInfo = data.results.artistmatches.artist.map((p) => {
+        // for (let i = 0; i < artists.results.artistmatches.artist.length; i++) {
+        const artistInFo = await artists.results.artistmatches.artist.map(
+            (p) => {
                 return {
                     name: p.name,
                     mbid: p.mbid,
                     url: p.url,
                     image_small: p.image[0]["#text"],
-                    image: p.image[i],
+                    image: p.image,
                 };
+            }
+        );
+        // artistInfos.push(artistInFo);
+        // }
+        // let stringData = artistInfos.toString();
+        // async (req, res) => {
+        try {
+            const resultCsv = json2csv.parse(artistInFo);
+            // stringify(
+            //     artistInfos,
+            //     {
+            //         header: true,
+            //         columns: ["name", "mbid", "url", "image_small", "image"],
+            //     },
+            //     (err, output) => {
+            //         if (err) throw err;
+            //         else {
+            //             fs.writeFile(artistFilePath, output, (err) => {
+            //                 if (err) throw err;
+            //             });
+            //         }
+            //     }
+            // );
+            fs.writeFileSync(`${artistName}.csv`, resultCsv);
+            console.log("성공!", `${artistName}.csv`);
+            return res.status(200).json({
+                message: "Success! Please check downloaded csv.file.",
             });
-            console.log("artists", artistInfo);
+        } catch {
+            console.log("실패!");
+            return res.json({ message: "something went wrong" });
         }
-    });
+        // };
+    };
+
+    // // console.log(...artistInfos);
+    // const data = artistInfos.map((row) => row.join(",")).join("\n");
+    // fs.writeFileSync(`${artistName}.csv`, data);
+    // return;
+    // };
+})();
